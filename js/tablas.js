6970eaa3cb9eb6798224c21097ef41398444e7f5
Juan Pablo Montoya <juan.montoyao@upb.edu.co>
2023-02-25 19:23:51 -0500

    Agregada la funcionalidad en medicamentos

--- js/tablas.js
+++ js/tablas.js
@@ -1,10 +1,15 @@
 let citas = [];
+let medicamentos=[];
 let tablaCitas = document.getElementById("tablaCitas");
+let tablaMedicamentos = document.getElementById("tablaMedicamentos");
 
 function aggCita(obj){
   citas.push(obj);
 }
 
+function aggMedicamento(obj){
+    medicamentos.push(obj);
+  }
 
 window.onload=function(){
   aggCita({
@@ -17,10 +22,19 @@ window.onload=function(){
     hora: "4:00"
   });
 
+  aggMedicamento({
+    medicamento: "Acetaminofén",
+    hora: "8:00"
+  });
+
 localStorage.setItem("citas",JSON.stringify(citas));
   
   for(i in citas){
     tablaCitas.innerHTML += "<tr><td>"+citas[i].cita+"</td><td>"+citas[i].hora+"</td></tr>"
+  }
+
+  for(i in medicamentos){
+    tablaMedicamentos.innerHTML += "<tr><td>"+medicamentos[i].medicamento+"</td><td>"+medicamentos[i].hora+"</td></tr>"
   }
   
 
