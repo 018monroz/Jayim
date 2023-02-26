addbf48f6d87cd13dcf520478f342c28603e741d
Andersson <a.cardenas.quiros@gmail.com>
2023-02-26 05:03:26 -0500

    logica de archivo tablas casi terminada

--- js/tablas.js
+++ js/tablas.js
@@ -4,7 +4,7 @@ let tablaCitas = document.getElementById("tablaCitas");
 let tablaMedicamentos = document.getElementById("tablaMedicamentos");
 
 var tipoCita,tipoCita,descripcion,nomMedico,tipoMedico,hora,fecha,lugar,btn_enviar;
-
+var nomMed,cantidad,periodicidad,dias,horas,horamed,fecha,btn_enviarmed;
 function aggCita(obj){
   citas.push(obj);
 }
@@ -17,13 +17,57 @@ function aggMedicamento(obj){
 window.onload=init;
 
 function init(){
-
+  btn_enviarmed=document,getElementById("btn_enviarmed");
+  btn_enviarmed.addEventListener("click",enviarMed);
+  console.log(btn_enviarmed.id);
   btn_enviar=document.getElementById("btn_enviar");
-  btn_enviar.addEventListener("click",enviar);
+  btn_enviar.addEventListener("click",enviarCita);
+  console.log(btn_enviar.id);
+}
+
+function enviarMed(){
+  nomMed=document.getElementById("nomMed").value;
+  cantidad=document.getElementById("cantidad").value;
+  periodicidad=document.getElementById("periodicidad").value;
+  dias=document.getElementById("dias").value;
+  horas=document.getElementById("horas").value;
+  horamed=document.getElementById("horamed").value;
+  fecha=document.getElementById("fecha").value;
+
+  console.log(nomMed);
+  console.log(cantidad);
+  console.log(periodicidad);
+  console.log(dias);
+  console.log(horas);
+  console.log(horamed);
+  console.log(fecha);
+
+
+  document.getElementById("nomMed").value="";
+  document.getElementById("cantidad").value="";
+  document.getElementById("periodicidad").value="";
+  document.getElementById("dias").value="";
+  document.getElementById("horas").value="";
+  document.getElementById("horamed").value="";
+  document.getElementById("fecha").value="";
+  
+
+  var obj={
+    Nombre_medico: nomMed,
+    Cantidad: cantidad,
+    Periodicidad: periodicidad,
+    Dias: dias, 
+    Horas: horas,
+    Horamed: horamed,
+    Fecha: fecha
+  }
+  aggMedicamento(obj);
+  localStorage.setItem("Medicamento "+(localStorage.length+1),JSON.stringify(medicamentos));
+
 
 }
 
-function enviar(){
+function enviarCita(){
   tipoCita=document.getElementById("tipoCita").value;
   descripcion=document.getElementById("descripcion").value;
   nomMedico=document.getElementById("nomMedico").value;
@@ -53,7 +97,6 @@ function enviar(){
             "Lugar: "+lugar;
   
   console.log(texto);*/
-  
   var obj={
     Tipo_cita: tipoCita,
     Descripcion: descripcion,
@@ -64,8 +107,7 @@ function enviar(){
     Hora: hora
   }
   aggCita(obj);
-  localStorage.setItem("cita"+(localStorage.length+1),JSON.stringify(citas));
-  
+  localStorage.setItem("cita "+(localStorage.length+1),JSON.stringify(citas));
 }
 
 /*
