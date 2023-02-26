e7c947029cd58d074dd6262ab2324379334d1701
Juan Pablo Montoya <juan.montoyao@upb.edu.co>
2023-02-25 19:05:21 -0500

    intento de llenado dinámico de tabla en main

---
+++ js/tablas.js
@@ -0,0 +1,28 @@
+let citas = [];
+let tablaCitas = document.getElementById("tablaCitas");
+
+function aggCita(obj){
+  citas.push(obj);
+}
+
+
+window.onload=function(){
+  aggCita({
+    cita: "Otorrino",
+    hora: "7:30"
+  });
+  
+  aggCita({
+    cita: "Odontologo",
+    hora: "4:00"
+  });
+
+localStorage.setItem("citas",JSON.stringify(citas));
+  
+  for(i in citas){
+    tablaCitas.innerHTML += "<tr><td>"+citas[i].cita+"</td><td>"+citas[i].hora+"</td></tr>";
+  }
+  
+
+
+}
