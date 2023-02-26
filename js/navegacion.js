b926af44758f4a0c844466abe191750f005e7973
Andersson <a.cardenas.quiros@gmail.com>
2023-02-26 04:41:42 -0500

    local storage almacenando

--- js/navegacion.js
+++ js/navegacion.js
@@ -1,11 +1,9 @@
 
 
 function inicializar(event){
-
     var cadena=event.target.id.toString();
     var array= cadena.split("_");
     var pagina=array[1]+".html";
-    location.href=pagina;
-    
+    location.href=pagina;   
 }
 
--- js/tablas.js
+++ js/tablas.js
@@ -3,14 +3,72 @@ let medicamentos=[];
 let tablaCitas = document.getElementById("tablaCitas");
 let tablaMedicamentos = document.getElementById("tablaMedicamentos");
 
+var tipoCita,tipoCita,descripcion,nomMedico,tipoMedico,hora,fecha,lugar,btn_enviar;
+
 function aggCita(obj){
   citas.push(obj);
 }
 
 function aggMedicamento(obj){
     medicamentos.push(obj);
-  }
+}
 
+
+window.onload=init;
+
+function init(){
+
+  btn_enviar=document.getElementById("btn_enviar");
+  btn_enviar.addEventListener("click",enviar);
+
+}
+
+function enviar(){
+  tipoCita=document.getElementById("tipoCita").value;
+  descripcion=document.getElementById("descripcion").value;
+  nomMedico=document.getElementById("nomMedico").value;
+  tipoMedico=document.getElementById("tipoMedico").value;
+  hora=document.getElementById("hora").value;
+  fecha=document.getElementById("fecha").value;
+  lugar=document.getElementById("lugar").value;
+  
+
+  document.getElementById("tipoCita").value="";
+  document.getElementById("descripcion").value="";
+  document.getElementById("nomMedico").value="";
+  document.getElementById("tipoMedico").value="";
+  document.getElementById("hora").value="";
+  document.getElementById("fecha").value="";
+  document.getElementById("lugar").value="";
+
+
+
+
+  /*
+  var texto="Tipo de cita: "+tipoCita+"\n"+
+            "Descripción: "+descripcion+"\n"+
+            "Nombre del medico: "+nomMedico+"\n"+
+            "Tipo de medico: "+tipoMedico+"\n"+
+            "Fecha: "+fecha+"\n"+
+            "Lugar: "+lugar;
+  
+  console.log(texto);*/
+  
+  var obj={
+    Tipo_cita: tipoCita,
+    Descripcion: descripcion,
+    NomMedico: nomMedico,
+    Tipo_medico: tipoMedico, 
+    Fecha: fecha,
+    Lugar: lugar,
+    Hora: hora
+  }
+  aggCita(obj);
+  localStorage.setItem("cita"+(localStorage.length+1),JSON.stringify(citas));
+  
+}
+
+/*
 window.onload=function(){
   aggCita({
     cita: "Otorrino",
@@ -39,4 +97,4 @@ localStorage.setItem("citas",JSON.stringify(citas));
   
 
 
-}
+*/
