94c1ea9e8acb51709a0195e5f6fc204e01a62bf4
Juan Pablo Montoya <juan.montoyao@upb.edu.co>
2023-02-26 23:18:38 -0500

    Generacion de archivo de gulpfile, js minificado y redirección de index

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const {src,dest} = require('gulp');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+
+function js(){
+	return src("./src/js/**/*.js")
+		.pipe(concat("main.min.js"))
+        .pipe(uglify())
+		.pipe(dest('./src/js'));
+}
+
+exports.js = js;
