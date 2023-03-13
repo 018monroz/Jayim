e54d26db3252c04acb36dfe04768914bbc00e1a0
Andersson <a.cardenas.quiros@gmail.com>
2023-03-12 23:46:02 -0500

    prueba 17 splash en githubPages

--- src/js/splash.js
+++ src/js/splash.js
@@ -1,5 +1,5 @@
 const preloaderEnd = document.getElementById("preloader-end");
 
 preloaderEnd.addEventListener("animationend", () => {
-  location.href="./src/main.html";
+  location.href="main.html";
 });
